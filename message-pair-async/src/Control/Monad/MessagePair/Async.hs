2e93717f747a7df88d976b070dad02fa0568b80a
edwardwas <ed@wastell.co.uk>
2018-04-14 23:34:50 +0100

    Wrote async to work with embeding

--- message-pair-async/src/Control/Monad/MessagePair/Async.hs
+++ message-pair-async/src/Control/Monad/MessagePair/Async.hs
@@ -1,75 +1,60 @@
-{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE UndecidableInstances       #-}
 
 module Control.Monad.MessagePair.Async where
 
 import           Control.Monad.MessagePass
 import           Control.Monad.ServerOrClient
 
-import           Control.Concurrent.Async
+import           Control.Concurrent.Async.Lifted.Safe
 import           Control.Concurrent.STM
-import           Control.Monad.Base
 import           Control.Monad.Reader
-import           Control.Monad.State
 import           Control.Monad.Trans.Control
-import           Control.Monad.Writer
-import qualified Data.ByteString              as B
+import qualified Data.ByteString                      as B
 import           Data.Serialize
 
--- | And instance of `MonadMessage` that runs in two threads on a local system. This is of limited utility, but is simple to understand.
 newtype MessageAsyncT m a = MessageAsyncT
-    { unMessageAsyncT :: ReaderT (TChan B.ByteString, TChan B.ByteString) m a
-    } deriving ( Functor
-               , Applicative
-               , Monad
-               , MonadIO
-               , MonadState s
-               , MonadWriter w
-               , MonadTrans
-               )
-
-instance MonadReader r m => MonadReader r (MessageAsyncT m) where
-  ask = MessageAsyncT $ lift ask
-  local func (MessageAsyncT act) = do
-      chans <- MessageAsyncT ask
-      MessageAsyncT $ lift $ local func $ runReaderT act chans
+    { unMessageAsyncT :: ReaderT (TChan B.ByteString, TChan (B.ByteString -> STM ())) m a
+    } deriving (Functor,Applicative,Monad, MonadIO)
 
 instance MonadIO m => MonadMessage (MessageAsyncT m) where
-  sendMessage a = MessageAsyncT $ do
-    (chan,_) <- ask
-    liftIO $ atomically $ writeTChan chan $ encode a
-  waitForMessage = MessageAsyncT $ do
-    (_,chan) <- ask
-    msg <- liftIO $ atomically $ readTChan chan
-    case decode msg of
-      Right a -> return a
+  sendMessage a = do
+    (sendChan,_) <- MessageAsyncT ask
+    liftIO $ atomically $ writeTChan sendChan $ encode a
+  waitForMessage = do
+    (_,waitChan) <- MessageAsyncT ask
+    waitVar <- liftIO $ atomically $ newEmptyTMVar
+    liftIO $ atomically $ writeTChan waitChan $ putTMVar waitVar
+    result <- liftIO $ atomically $ readTMVar waitVar
+    case decode result of
+      Right x -> return x
       Left e  -> error $ "Decode error: " ++ e
 
-instance MonadBase b m => MonadBase b (MessageAsyncT m) where
-  liftBase = lift . liftBase
+runMessageAsyncT ::
+       (MonadBaseControl IO m, Forall (Pure m), MonadIO m)
+    => TChan B.ByteString
+    -> TChan B.ByteString
+    -> MessageAsyncT m a
+    -> m ()
+runMessageAsyncT sendChan recvChan (MessageAsyncT act) = do
+    waitChan <- liftIO $ atomically $ newTChan
+    let recvHelper =
+            liftIO (atomically (readTChan waitChan <*> readTChan recvChan)) >>=
+            liftIO . atomically
+    recvSync <- async $ forever recvHelper
+    runReaderT act (sendChan, waitChan)
+    cancel recvSync
 
-instance MonadTransControl MessageAsyncT where
-  type StT MessageAsyncT a = StT (ReaderT (TChan B.ByteString, TChan B.ByteString)) a
-  liftWith = defaultLiftWith MessageAsyncT unMessageAsyncT
-  restoreT = defaultRestoreT MessageAsyncT
-
-instance MonadBaseControl b m => MonadBaseControl b (MessageAsyncT m) where
-  type StM (MessageAsyncT m) a = ComposeSt MessageAsyncT m a
-  liftBaseWith = defaultLiftBaseWith
-  restoreM = defaultRestoreM
-
--- | Run a `ServerOrClient` program on both the client and the server
-runMessageAsyncPair ::
-       (forall location. SLocationI location =>
-                             ServerOrClient (MessageAsyncT IO) (MessageAsyncT IO) location ())
-    -> IO ()
-runMessageAsyncPair act = do
-    (chanA, chanB) <- atomically ((,) <$> newTChan <*> newTChan)
-    clientSync <-
-        async $ runReaderT (unMessageAsyncT (unOnClient act)) (chanA, chanB)
-    runReaderT (unMessageAsyncT $ unOnServer act) (chanB, chanA)
+runMessageServerOrClient ::
+       (MonadIO m, MonadBaseControl IO m, Forall (Pure m))
+    => (forall location. SLocationI location =>
+                             ServerOrClient (MessageAsyncT m) (MessageAsyncT m) location ())
+    -> m ()
+runMessageServerOrClient app = do
+    (chanA, chanB) <- liftIO $ atomically ((,) <$> newTChan <*> newTChan)
+    clientSync <- async $ runMessageAsyncT chanA chanB $ unOnClient app
+    runMessageAsyncT chanB chanA $ unOnServer app
     wait clientSync
