d8b98e469bdb3a6bef76b3da155ac4140ba76363
edwardwas <ed@wastell.co.uk>
2018-04-14 23:45:00 +0100

    More instances, but embed is more broken

--- message-pair-async/src/Control/Monad/MessagePair/Async.hs
+++ message-pair-async/src/Control/Monad/MessagePair/Async.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE UndecidableInstances       #-}
 
 module Control.Monad.MessagePair.Async where
 
@@ -11,6 +13,7 @@ import           Control.Monad.ServerOrClient
 
 import           Control.Concurrent.Async.Lifted.Safe
 import           Control.Concurrent.STM
+import           Control.Monad.Base
 import           Control.Monad.Reader
 import           Control.Monad.Trans.Control
 import qualified Data.ByteString                      as B
@@ -32,6 +35,29 @@ instance MonadIO m => MonadMessage (MessageAsyncT m) where
     case decode result of
       Right x -> return x
       Left e  -> error $ "Decode error: " ++ e
+
+instance MonadReader r m => MonadReader r (MessageAsyncT m) where
+  ask = MessageAsyncT $ lift ask
+  local func (MessageAsyncT act) = do
+      chans <- MessageAsyncT ask
+      MessageAsyncT $ lift $ local func $ runReaderT act chans
+
+instance MonadTrans MessageAsyncT where
+  lift = MessageAsyncT . lift
+
+instance MonadBase b m => MonadBase b (MessageAsyncT m) where
+  liftBase = lift . liftBase
+
+instance MonadTransControl MessageAsyncT where
+    type StT MessageAsyncT a = StT (ReaderT ( TChan B.ByteString
+                                            , TChan (B.ByteString -> STM ()))) a
+    liftWith = defaultLiftWith MessageAsyncT unMessageAsyncT
+    restoreT = defaultRestoreT MessageAsyncT
+
+instance MonadBaseControl b m => MonadBaseControl b (MessageAsyncT m) where
+  type StM (MessageAsyncT m) a = ComposeSt MessageAsyncT m a
+  liftBaseWith = defaultLiftBaseWith
+  restoreM = defaultRestoreM
 
 runMessageAsyncT ::
        (MonadBaseControl IO m, Forall (Pure m), MonadIO m)
@@ -58,3 +84,8 @@ runMessageServerOrClient app = do
     clientSync <- async $ runMessageAsyncT chanA chanB $ unOnClient app
     runMessageAsyncT chanB chanA $ unOnServer app
     wait clientSync
+
+test :: SLocationI location => ServerOrClient (MessageAsyncT IO) (MessageAsyncT IO) location ()
+test = do
+  getLineAct <- embedOnClient (\str -> liftIO (putStrLn str >> getLine))
+  runOnServer_ $ liftIO $ getLineAct "Boop!" >>= print
